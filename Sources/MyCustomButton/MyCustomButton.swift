1a4aa0e3f3082f9c119c5ab79877d98ceace62e6
SandlerRodrigo <rodrigoqsandler@gmail.com>
2025-10-24 23:42:31 -0300

    First Commit

--- Sources/MyCustomButton/MyCustomButton.swift
+++ Sources/MyCustomButton/MyCustomButton.swift
@@ -1,2 +1,20 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+public struct CustomButton: View {
+    private let title: String
+    private let action: () -> Void
+    
+    public init(title: String, action: @escaping () -> Void) {
+        self.title = title
+        self.action = action
+    }
+    
+    public var body: some View {
+        Button(action: action) {
+            Text(title)
+        }
+    }
+}
